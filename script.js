0d6323476bcd8821f0be9d439d7037284b9ec3cb
Tarek Hamaoui <tarek.m.hamaoui@gmail.com>
2021-02-09 11:27:30 +0100

    adds leflet library for interactive map

--- script.js
+++ script.js
@@ -11,3 +11,39 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// GEOLOCATION API -BROWSER API  LIKE INTERNATIONALISATION/OBSERVER API
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    //takes 2 calback functions, first success callback, second error callback
+    position => {
+      console.log(position); //gives position object with latitude and longitude
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+
+      //   const latitude = position.coords.latitude;
+      //   const longitude = position.coords.longitude;
+      //   console.log(latitude, longitude);
+
+      //using destructuring
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.de/maps/@${latitude},${longitude},16z`);
+      const map = L.map('map').setView([51.505, -0.09], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    () => {
+      alert('Could not get position');
+    }
+  );
+}
